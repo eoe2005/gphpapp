d332c5ace7307297578758bc07570767232354b5
吃掉大象的蚂蚁 <eoe2005@qq.com>
2024-08-12 22:40:58 +0800

    fx

--- App/Apps/Admin/Controllers/IndexController.php
+++ App/Apps/Admin/Controllers/IndexController.php
@@ -2,48 +2,13 @@
 
 namespace App\Apps\Admin\Controllers;
 
-use App\Models\AdminModel;
+
 use G\Controller;
-use G\Req;
+use G\Logic\Controllers\AdminLoginTrait;
+
 
 class IndexController extends Controller
 {
 
-    /**
-     * 账号登录
-     * @return array
-     * @throws \Exception
-     */
-    function loginAction()
-    {
-        $name = Req::input('login_name');
-        $pass = Req::input('pass');
-        if (!$name || !$pass) {
-            throw new \Exception('账号或者密码错误', 403);
-        }
-        $row = AdminModel::where(['login_name' => $name])->fetch();
-        if (!$row || $row['passwd'] != AdminModel::buildPass($pass, $row['sign'])) {
-            throw new \Exception('账号或者密码错误', 403);
-        }
-        if ($row['status'] != 0) {
-            throw new \Exception('账号禁止登录', 403);
-        }
-        $_SESSION['admin_id'] = $row['id'];
-        return [
-            'nick_name' => $row['nick_name'],
-            'avatar' => $row['avatar'],
-        ];
-    }
-
-
-    /**
-     * 退出登录
-     * @return array
-     */
-    function logoutAction()
-    {
-        session_destroy();
-        return [];
-    }
-
+    use AdminLoginTrait;
 }
--- App/Apps/Admin/Controllers/SysController.php
+++ App/Apps/Admin/Controllers/SysController.php
@@ -2,50 +2,11 @@
 
 namespace App\Apps\Admin\Controllers;
 
-use App\Models\AdminModel;
-use App\Services\SysConfSvc;
-use G\Req;
+
+use G\Logic\Controllers\AdminSysTrait;
 
 class SysController extends BaseAdmin
 {
-    /**
-     * 获取配置信息
-     * @return array|mixed|string
-     */
-    function getConfAction()
-    {
-        return SysConfSvc::getConf(Req::input('key'),Req::input('defval'));
-    }
-
-    /**
-     * 保存卑职信息
-     * @return true
-     * @throws \Exception
-     */
-    function saveConfAction(){
-        return SysConfSvc::setConf(Req::input('key'),Req::input('data'));
-    }
-
-    /**
-     * 修改密码
-     * @return void
-     */
-    function changePwdAction(){
-        $oldPass = Req::input('old');
-        $pass = Req::input('pass');
-        if(!$oldPass || !$pass){
-            throw new \Exception('参数错误',403);
-        }
-        $row = AdminModel::find($this->getAdminId());
-        if($row['passwd'] != AdminModel::buildPass($oldPass,$row['sign'])){
-            throw new \Exception('旧密码错误',403);
-        }
-        $sign = AdminModel::genrateSign();
-        return AdminModel::update([
-            'passwd' => AdminModel::buildPass($pass,$sign),
-            'sign' => $sign
-        ],['id' => $this->getAdminId()]);
-    }
-
+    use AdminSysTrait;
 
 }
